f4f2ed9f101823b4d4ddcf4455d0ff36edbd0f37
kubukoz <kubukoz@gmail.com>
2014-10-13 18:38:10 +0200

    changes to musicPlayer, probably finished playing system

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ app.directive("galleryModal", function(ngDialog){
 app.directive("skrollrEnabled", function(){
     return{
         link: function(){
-            if(!(/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+            if(!(/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/).test(navigator.userAgent.toLowerCase() || navigator.vendor.toLowerCase() || window.opera.toLowerCase())){
                 var s = skrollr.init({smoothScrollingDuration:1, forceHeight: false});
             }
         }
@@ -68,16 +68,88 @@ app.directive("musicPlayer", function(){
     return{
         restrict: "AE",
         link: function(scope,elem,attr){
-            scope.playing = true;
-            scope.currentSong = "Isn't she lovely";
-            scope.switchPlaying = function(){
-                scope.playing=!scope.playing;
+            var p = scope.player = { playing: false, selected: 0, volume: 40, audio: new Audio()};
+            p.songs = [
+                {name: "Get Lucky", file: "dp.mp3"},
+                {name: "God Is Dead?", file: "bs.mp3"},
+                {name: "Feeling Good", file: "mb.mp3"},
+                {name: "Spierdalać Spać", file: "majda_budzik.mp3"},
+                {name: "God Is Dead? jeszcze raz", file: "bs.mp3"}
+            ];
+            p.audio.volume = p.volume/100;
+            p.audio.onended = function(){scope.$apply(function(){p.nextSong()})};
+            scope.$watch(function(){return p.volume}, function(v){if(p.audio)p.audio.volume = v/100});
+                                p.audio.src = "music/"+p.songs[p.selected].file;
+            p.updatePlaying = function(pl){
+                p.playing=pl;
+                if(pl){
+                    if(p.audio.src.indexOf("music/"+ p.songs[p.selected].file)<0)
+                        p.audio.src = "music/"+ p.songs[p.selected].file;
+                    p.audio.play();
+                }
+                else{
+                    p.audio.pause();
+                }
             }
-            scope.nextSong = function(){
-
+            p.switchPlaying = function(){
+                p.updatePlaying(!p.playing);
             }
-            scope.previousSong = function(){
+            p.nextSong = function(){
+                p.selected+=(p.selected<p.songs.length-1?1:-(p.songs.length-1));
+                p.updatePlaying(true);
+            }
+            p.previousSong = function(){
+                p.selected-=(p.selected>0?1:-(p.songs.length-1));
+                p.updatePlaying(true);
+            }
+            p.volumeUp = function(){
+                p.volume = Math.min(100, p.volume+10);
+            }
+            p.volumeDown = function(){
+                p.volume = Math.max(0, p.volume-10);
+            }
 
+            var scrollbar = elem.children()[0].children[2].children[1];
+            var clicked = false;
+            scrollbar.onmousedown = function(e){
+                clicked = true;
+                scrollbar.onmousemove(e);
+            }
+            document.onmouseup = function(){
+                clicked = false;
+            }
+            scrollbar.onmousemove = function(e){
+                if(clicked){
+                    e.preventDefault();
+                    var vol = Math.round(100*Math.min(Math.max((e.offsetX || (e.clientX-scrollbar.getBoundingClientRect().left) || 0), 0)/ scrollbar.offsetWidth, 1));
+                    scope.$apply(function(){
+                        p.volume = vol;
+                    })
+                }
+            }
+            document.onkeydown = function(e){
+                scope.$apply(function(){
+                    switch(e.keyCode){
+                        case 37:
+                            p.previousSong();
+                            break;
+                        case 39:
+                            p.nextSong();
+                            break;
+                        case 38:
+                            if(e.shiftKey)
+                                p.volumeUp();
+                            break;
+                        case 40:
+                            if(e.shiftKey)
+                                p.volumeDown();
+                            break;
+                        case 32:
+                            e.preventDefault();
+                            p.switchPlaying();
+                            break;
+                    }
+                });
             }
         }
     }
