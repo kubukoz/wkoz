0166f914084ff5592cef9df929dcdf9cf9f8ad30
Jakub Kozłowski <kubukoz@gmail.com>
2016-09-30 17:59:38 +0200

    Cleanup for travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,38 @@
-var gulp = require('gulp'),
-    ngAnnotate = require("gulp-ng-annotate");
-autoprefixer = require('gulp-autoprefixer'),
-    livereload = require('gulp-livereload'),
-    rename = require("gulp-rename"),
-    uglify = require("gulp-uglify"),
-    minifyCSS = require("gulp-minify-css"),
-    sass = require('gulp-sass');
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
+var rename = require("gulp-rename");
+var cleanCSS = require("gulp-clean-css");
+var sass = require('gulp-sass');
 
-gulp.task("uglify", function(){
-    gulp.src("js/main.js")
-        .pipe(ngAnnotate())
-        .pipe(uglify({outSourceMap: true}))
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest('js'));
+gulp.task('styles', function () {
+  return gulp.src('styles/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'safari 5', 'ios 6', 'android 4']
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('styles/'))
+    .pipe(livereload())
 });
 
-gulp.task('styles', function() {
-    livereload.listen();
-    return gulp.src('styles/*.scss')
-        .pipe(sass({onError: function(err){
-            console.log(err);
-        }}))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'safari 5', 'ios 6', 'android 4']
-        }))
-        .pipe(minifyCSS())
-        .pipe(livereload())
-        .pipe(gulp.dest('styles/'))
+gulp.task('deploy', ['styles'], () => {
+  return gulp.src([
+    "api/**",
+    "bower_components/**",
+    "favicon/favicon.png",
+    "images/**",
+    "js/**",
+    "plugins/**",
+    "styles/*.css",
+    "templates/**",
+    "keybase.txt",
+    "index.html"
+  ], {base: "."})
+    .pipe(gulp.dest("public/"))
 });
-gulp.task("watch", function(){
-    livereload.listen();
-    gulp.watch('styles/**', ['styles']);
-    //gulp.watch('js/**', ['uglify']);
+
+gulp.task("watch", () => {
+  livereload.listen();
+  gulp.watch('styles/*.scss', ['styles']);
 });
