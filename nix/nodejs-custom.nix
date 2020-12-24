352f528386ba5bb2538fbe69cd6cea27056d7ef3
Jakub Kozłowski <kubukoz@gmail.com>
2020-12-24 02:30:33 +0100

    Use nixjs

--- nix/nodejs-custom.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, version, sha256 }:
-
-with import pkgs { };
-callPackage (pkgs + "/pkgs/development/web/nodejs/nodejs.nix") {
-  inherit openssl icu;
-  python = python2;
-} { inherit version sha256; }
-
--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 { sources ? import ./nix/sources.nix }:
 
 let
-  pkgs = import sources.nixpkgs { };
-  node9 = (import ./nix/nodejs-custom.nix {
-    pkgs = sources.nixpkgs;
-    version = "9.10.1";
-    sha256 = "1widvxbc8sp8p8vp7q38b3zy0w1nx4iaqmp81s6bvaqs08h7wfy9";
-  });
-in pkgs.mkShell { buildInputs = [ node9 ]; }
+  nixpkgs = sources.nixpkgs;
+  nixjs-overlay = import sources.nixjs {
+    inherit nixpkgs;
+    versions = { nodejs = "9.10.1"; };
+  };
+  pkgs = import nixpkgs { overlays = [ nixjs-overlay ]; };
+
+in pkgs.mkShell { buildInputs = [ pkgs.nodejs ]; }
