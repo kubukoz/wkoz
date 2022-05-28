62d4724cebc70a02f0f3ef360975f6a7433da5a5
Jakub Kozłowski <kubukoz@gmail.com>
2022-05-28 04:35:14 +0200

    Merge header into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
-import { Header } from './Header';
+import { FC } from "react";
+import { Nav } from "./Nav";
+import { ReactComponent as Logo } from "./logo.svg";
+
+export const Header: FC = () => {
+  // todo
+  return (
+    <header id="header" scrollspy-broadcast scrollspy-offset="auto">
+      <div className="inside ninesixzero clearfix">
+        <a href="#about" du-smooth-scroll /* offset="{{duOffset}}" */>
+          <div id="logo_main">
+            <Logo />
+          </div>
+        </a>
+        <Nav />
+      </div>
+    </header>
+  );
+};
 
 const App = () => (
   <>
