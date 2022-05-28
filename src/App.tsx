3d2b34e22fe701ae8fc0499384d00ef3f89668e5
Jakub Kozłowski <kubukoz@gmail.com>
2022-05-28 04:37:56 +0200

    Implement "about"

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,40 @@ export const Header: FC = () => {
   );
 };
 
+const About = () => {
+  const description = `Muzyka w moim życiu odgrywa ogromną rolę.
+  Kiedyś, jako słuchacz, obecny byłem zarówno na wielu
+  festiwalach,
+  jak i koncertach w małych klubach. Obecnie - jako muzyk - podobnie, występowałem na dużych i małych
+  scenach.
+  Wieloletnie obserwacje życia muzycznego bardzo pomagają mi w relacjach między muzyką a jej
+  odbiorcami.`;
+
+  return (
+    <div
+      id="about"
+      scrollspy-broadcast
+      clickable-hidenav
+      scrollspy-offset="auto|60"
+    >
+      <div id="about_inside" className="ninesixzero clearfix">
+        <div className="image col-xs-12 col-sm-4">
+          <div className="inside"></div>
+        </div>
+        <div className="text col-xs-12 col-sm-8">
+          <h1>Włodek Kozłowski</h1>
+          <hr />
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => (
   <>
     <Header />
+    <About />
   </>
 );
 
